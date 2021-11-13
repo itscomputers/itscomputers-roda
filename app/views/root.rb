c4c10ccc8b462d84dbaf68fbf601e142e695c012
itscomputers <its_computers@protonmail.com>
2021-11-13 10:27:06 -0700

    add header and footer

--- app/views/root.rb
+++ app/views/root.rb
@@ -4,7 +4,7 @@ module Views
 
     LINKS = [
       Link.new("advent of code", "https://github.com/itscomputers/advent_of_code"),
-      Link.new("is it friday the 13th??", "https://github.com/itscomputers/friday-the-13th"),
+      Link.new("is it friday the 13th??", "https://fridaythe13th.itscomputers.lol"),
       Link.new("archimedes cattle problem", "/archimedes_cattle_problem"),
       Link.new("ebe - number theory for programmers", "/ebe"),
     ]
